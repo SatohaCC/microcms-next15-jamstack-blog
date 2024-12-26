7029c5dd2969306954a1bdbfe6fd3fcae2ee924b
satohaCC <un.cappuccinooo.per.favore+github@gmail.com>
2024-12-27 00:34:12 +0900

    fix: update panda.config.ts to include button recipe and refine include paths

--- panda.config.ts
+++ panda.config.ts
@@ -1,20 +1,31 @@
 import { defineConfig } from "@pandacss/dev";
 
+import { buttonRecipe } from "@/app/_ui/Button/Button.recipe";
+
 export default defineConfig({
     // Whether to use css reset
     preflight: true,
 
     // Where to look for your css declarations
-    include: ["./src/**/*.{js,jsx,ts,tsx}", "./pages/**/*.{js,jsx,ts,tsx}"],
+    include: [
+        "./src/_ui/**/*.{ts,tsx,js,jsx}",
+        "./src/**/*.{ts,tsx,js,jsx}",
+        "./pages/**/*.{ts,tsx,js,jsx}",
+    ],
 
     // Files to exclude
     exclude: [],
 
     // Useful for theme customization
     theme: {
-        extend: {},
+        extend: {
+            recipes: {
+                button: buttonRecipe,
+            },
+        },
     },
 
+    jsxFramework: "react",
     // The output directory for your css system
     outdir: "styled-system",
 });
